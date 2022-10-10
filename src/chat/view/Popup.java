f9f71f5bf8b055c645fff41483c2907fdeb94729
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-10-10 07:45:36 -0600

    popup added

--- src/chat/view/Popup.java
+++ src/chat/view/Popup.java
@@ -1,5 +1,24 @@
 package chat.view;
 
-public class Popup {
+import javax.swing.JOptionPane;
 
+public class Popup 
+{
+	public void displayMessage(String message)
+	{
+		JOptionPane.showInternalMessageDialog(null, message);
+	}
+	
+	public String askQuestion(String question)
+	{
+		String response = "";
+		response = JOptionPane.showInputDialog(null, question);
+		
+		if (response == null)
+		{
+			response = "";
+		}
+		
+		return response;
+	}
 }

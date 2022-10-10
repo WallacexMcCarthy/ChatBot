c6772cbd167cd58581aa723c86e818caa0e4267f
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-10-10 08:32:36 -0600

    all stubs added

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -16,4 +16,32 @@ public class Chatbot
 		String output = input;
 		return output;
 	}
+	private boolean spookyChecker(String text)
+	{
+		boolean isSpooky = true;
+		
+		return isSpooky;
+	}
+	private String spookyResponse()
+	{
+		String response = "That was really spooky!";
+		
+		return response;
+	}
+	private boolean isValidHTMLChecker(String text)
+	{
+		boolean isValid = true;
+		
+		return isValid;
+	}
+	private String isValidHTMLResponse()
+	{
+		String response = "This is a valid HTML!";
+		return response;
+	}
+	private String reversePronounDirection(String source)
+	{
+		String response = "";
+		return response;
+	}
 }

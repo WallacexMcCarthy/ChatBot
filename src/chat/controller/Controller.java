372fb4b6547e6175be56d8ecb2da208c9d4e9148
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-11-01 08:53:31 -0600

    notes

--- src/chat/controller/Controller.java
+++ src/chat/controller/Controller.java
@@ -6,6 +6,10 @@ import chat.model.Chatbot;
 
 import chat.view.Popup;
 
+// null pointer exception is when you are trying to do something on a null value, like getting the length of a null value string
+/* helper methods are all the methods that help the program do wht it needs to do, and they primarly halpe one method do something 
+ that it needs to do. in this example, all otehr methods help the process test method so all of those other methods are helper methods. 
+ */
 
 public class Controller 
 {
@@ -14,8 +18,8 @@ public class Controller
 	
 	public Controller()
 	{
-		this.chatbot = new Chatbot("Super smart chatbot");
-		this.view = new Popup();
+		this.chatbot = new Chatbot("Super smart chatbot"); // using the new keyword to initialize
+		this.view = new Popup(); // an overlaoded contructor means to have a class with multiple methods that are called the same thing but have different parameters.
 	}
 	public void start()
 	{
